ff9440e85e8519467e5ecf6717f6ffabaea04a5d
Joao Paulo <joaopaulowessler@gmail.com>
2021-03-11 21:58:31 -0300

    tela login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,6 +8,8 @@
         private $tpl;
         private $options = [];
         private $defaults = [
+            "header"=>true,
+            "footer"=>true,
             "data"=>[]
         ];
 
@@ -27,7 +29,7 @@
 
             $this->setData($this->options["data"]);
 
-            $this->tpl->draw("header");
+            if ($this->options["header"] === true) $this->tpl->draw("header");
         }
 
         private function setData($data = array()){
@@ -42,7 +44,7 @@
         }
 
         public function __destruct() {
-            $this->tpl->draw("footer");
+            if ($this->options["footer"] === true) $this->tpl->draw("footer");
         }
     }
 ?>
